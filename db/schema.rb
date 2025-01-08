1d08dc3ab08188e04255f7ec5298914760bbb591
Lucas Rossi <lucasrossi@fetch.ly>
2025-01-08 07:50:58 -0300

    add new columns to movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_06_110445) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_08_103759) do
   create_table "movies", force: :cascade do |t|
     t.string "name"
     t.string "rating"
     t.decimal "total_gross"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.date "released_on"
   end
 end
